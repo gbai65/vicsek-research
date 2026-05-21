6d278fd77e8f855b1a0365bd9edec2232963dc7c
Grace Bai <gracetbai@gmail.com>
2026-05-21 13:32:29 -0400

    reorganize

--- runtime/master.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=vicsek
-#SBATCH -n 5
-#SBATCH --time=7-00:00:00
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --partition=compute
-
-srun (runtime/runVicsek.sh)
